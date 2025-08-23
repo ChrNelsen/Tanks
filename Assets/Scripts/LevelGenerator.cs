deead13dcc98e1a033b795147b405df08deccb81
Chris Nelsen <ChrNelsen@gmail.com>
2025-08-23 07:10:05 -0500

    Level Generation

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,119 +1,57 @@
-using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
+using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject blockPrefab;
-    [SerializeField] private int width = 22;
-    [SerializeField] private int height = 16;
-    private Dictionary<RoomType, Vector2Int[]> roomShapes;
+    [SerializeField] private int floorWidth = 3;
+    [SerializeField] private int floorHeight = 6;
+    [SerializeField] private int roomBorder = 1;
+    [SerializeField] private int totalRooms = 5;
+
+    private List<Vector2> spawnedRooms = new List<Vector2>();
+    private Room roomBuilder;
 
     private void Start()
     {
-        // Define room shapes in grid coords
-        roomShapes = new Dictionary<RoomType, Vector2Int[]>
-        {
-            { RoomType.Normal, new Vector2Int[] { new Vector2Int(0, 0) } },
-        };
+        roomBuilder = new Room(blockPrefab, transform);
 
-        float xOffset = (width - 1) / 2f;
-        float zOffset = (height - 1) / 2f;
-        //GenerateRoom(new Vector2Int(0, 0), RoomType.Big, xOffset, zOffset);
-        GenerateRoom(new Vector2Int(0, 0), RoomType.LShaped, xOffset, zOffset);
+        Vector2 startPos = Vector2.zero;
+        roomBuilder.GenerateRoom(RoomType.Normal, startPos, floorWidth, floorHeight);
+        spawnedRooms.Add(startPos);
 
+        for (int i = 1; i < totalRooms; i++)
+            SpawnNextRoom();
     }
-    private void GenerateRoom(Vector2Int startPos, RoomType type, float xOffset, float zOffset)
+
+    private void SpawnNextRoom()
     {
-        if (type == RoomType.Normal)
+        Vector2 baseRoom = spawnedRooms[Random.Range(0, spawnedRooms.Count)];
+        Vector2 newPos = GetAdjacentPosition(baseRoom, (Direction)Random.Range(0, 4));
+
+        if (!spawnedRooms.Contains(newPos))
         {
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int z = 0; z < height; z++)
-                {
-                    Vector3 position = new Vector3(x - xOffset, -1, z - zOffset);
-
-                    // Raise border blocks to 0 height, ground blocks stay at -1
-                    float yPos = IsBorder(x, z) ? 0f : position.y;
-                    Instantiate(blockPrefab, new Vector3(position.x, yPos, position.z), Quaternion.identity);
-                }
-            }
+            roomBuilder.GenerateRoom(RoomType.Normal, newPos, floorWidth, floorHeight);
+            spawnedRooms.Add(newPos);
         }
-
-        else if (type == RoomType.Long)
+        else
         {
-            // Long room: double width, same height
-            int longWidth = width * 2;
-
-            for (int x = 0; x < longWidth; x++)
-            {
-                for (int z = 0; z < height; z++)
-                {
-                    Vector3 position = new Vector3(x - (longWidth - 1) / 2f, -1, z - zOffset);
-                    float yPos = (x == 0 || x == longWidth - 1 || z == 0 || z == height - 1) ? 0f : position.y;
-                    Instantiate(blockPrefab, new Vector3(position.x, yPos, position.z), Quaternion.identity);
-                }
-            }
-        }
-
-        else if (type == RoomType.Big)
-        {
-            // Big room: double width and double height
-            int bigWidth = width * 2;
-            int bigHeight = height * 2;
-
-            for (int x = 0; x < bigWidth; x++)
-            {
-                for (int z = 0; z < bigHeight; z++)
-                {
-                    Vector3 position = new Vector3(x - (bigWidth - 1) / 2f, -1, z - (bigHeight - 1) / 2f);
-                    float yPos = (x == 0 || x == bigWidth - 1 || z == 0 || z == bigHeight - 1) ? 0f : position.y;
-                    Instantiate(blockPrefab, new Vector3(position.x, yPos, position.z), Quaternion.identity);
-                }
-            }
-        }
-
-else if (type == RoomType.LShaped)
-{
-    // L-shaped room: composed of 3 normal rooms (width x height each)
-    int normalWidth = width;
-    int normalHeight = height;
-
-    // Loop through total L room bounds
-    for (int x = 0; x < normalWidth * 2; x++)         // width = 2 normals
-    {
-        for (int z = 0; z < normalHeight * 2; z++)    // height = 2 normals
-        {
-            bool skip = (x >= normalWidth && z >= normalHeight); // top-right quarter is empty
-
-            Vector3 position = new Vector3(x - (normalWidth * 2 - 1) / 2f, -1, z - (normalHeight * 2 - 1) / 2f);
-
-            // Outer walls + inner walls along the cutout corner
-            bool isBorder = (x == 0 || x == normalWidth * 2 - 1 || z == 0 || z == normalHeight * 2 - 1) || 
-                            (skip && (x == normalWidth || z == normalHeight));
-
-            float yPos = isBorder ? 0f : position.y;
-
-            if (!skip || isBorder) // keep walls in cutout
-            {
-                Instantiate(blockPrefab, new Vector3(position.x, yPos, position.z), Quaternion.identity);
-            }
+            SpawnNextRoom(); // Retry if position is taken
         }
     }
-}
+
+    private Vector2 GetAdjacentPosition(Vector2 currentPos, Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.Up: return currentPos + new Vector2(0, floorHeight + roomBorder);
+            case Direction.Down: return currentPos - new Vector2(0, floorHeight + roomBorder);
+            case Direction.Left: return currentPos - new Vector2(floorWidth + roomBorder, 0);
+            case Direction.Right: return currentPos + new Vector2(floorWidth + roomBorder, 0);
+            default: return currentPos;
+        }
     }
 
-    private bool IsBorder(int x, int z)
-    {
-        return x == 0 || x == width - 1 || z == 0 || z == height - 1;
-    }
-
-    public enum RoomType
-    {
-        Normal,
-        Long,
-        Big,
-        LShaped
-    }
+    public enum RoomType { Normal, Long, Big, LShaped }
+    public enum Direction { Up, Down, Left, Right }
 }
